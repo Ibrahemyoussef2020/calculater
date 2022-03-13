a196bde39cbe087e805fe5217c86a99fb9b896ae
unknown <ibrahem.youssef2020@hotmail.com>
2022-03-13 19:09:44 +0200

    responsive

--- calculator.js
+++ calculator.js
@@ -1,38 +1,35 @@
-
+//////////////////////////////////////////////////
+//              buttons and screen             //
+/////////////////////////////////////////////////
 var myNumber = document.getElementsByClassName("bot"),
     mScreen =  document.getElementById("screen"),
-    myDelete =  document.getElementById("delete"),
-    myClose =  document.getElementById("close"),
-    myEqual =  document.getElementById("eq"),
     i  = 0;
 
-
 for (var i = 0; i < myNumber.length; i++) {
-	
-
   myNumber[i].onclick = function (){
 
-
-     document.getElementById("screen").value   += this.value ;
- }
+     document.getElementById("screen").value   +=  this.value ;
+  }
 
 }
- 
+//////////////////////////////////////////////////
+//              delete and close               //
+///////////////////////////////////////////////// 
+let myDelete =  document.getElementById("delete"),
+    myClose =  document.getElementById("close");
+
 myDelete.onclick  = function dellete(){
-
-	mScreen.value = "";
+	  mScreen.value = "";
 }
-
-
-
 
 myClose.onclick = function(){
 
 	mScreen.value = mScreen.value.slice(0,mScreen.value.length-1)
 }
-
-
-
+//////////////////////////////////////////////////
+//             equal  and enter                 //
+///////////////////////////////////////////////// 
+let myEqual =  document.getElementById("eq");
 
 myEqual.onclick = function (){
 
@@ -40,13 +37,10 @@ myEqual.onclick = function (){
 
        mScreen.placeholder = "write number";
 
-
      }else {
-
-       
+   
      	mScreen.value = eval(mScreen.value);
      }	
-
 }
 
 document.onkeydown = function(event) {
@@ -55,7 +49,7 @@ document.onkeydown = function(event) {
 
 	 var i = event.keyCode,
          
-         x = String.fromCharCode(i) ;
+       x = String.fromCharCode(i) ;
 
      
      if(mScreen.value === ""){
@@ -64,14 +58,27 @@ document.onkeydown = function(event) {
 
 
      }else if (i === 187){
-
-        
+    
      	mScreen.value = eval(mScreen.value);
 
-     }
+     }  
+}
+//////////////////////////////////////////////////
+//                  on and off                 //
+/////////////////////////////////////////////////
+let onOff = document.getElementById("on");
+
+  onOff.onclick = function(){
+    myDelete.click();
+
+    mScreen.classList.toggle("off");
+
+    for (i = 0; i < myNumber.length; i++) {
+  
+        myNumber[i].classList.toggle("disabled");
+    }
     
 }
-
 
 
 
